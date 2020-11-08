19d41a670c9283e0fe4e8547209cce2ca4cf6f2d
Emre Yılmaz <hintlikaraemre@gmail.com>
2020-11-08 11:01:54 +0300

    middleware

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const express = require("express")
 const app = express()
 
-
 const PORT = 3000;
+
+const customMiddleware = (req,res,next) => {
+    console.log("Sadece ali gönderebilir")
+    next()
+}
+
+//app.use(customMiddleware)
 
 app.get('/',(req,res) => {
     console.log("Home Page")
     res.send("Home Page")
 })
 
-app.get('/about',(req,res) => {
-    res.json({"merhaba":"deneme"})
+app.get('/createPost',customMiddleware,(req,res) => {
+    res.send("send a post")
 })
 
 app.listen(PORT,() => {
