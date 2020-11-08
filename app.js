f6b9a18679c4a1a74286f476e2842e35f3d52c47
Emre Yılmaz <hintlikaraemre@gmail.com>
2020-11-08 11:52:57 +0300

    user schema

--- app.js
+++ app.js
@@ -1,23 +1,31 @@
 const express = require("express")
 const app = express()
+const mongoose = require("mongoose")
+const {MONGOURI} = require("./keys.js")
+
 
 const PORT = 3000;
 
-const customMiddleware = (req,res,next) => {
-    console.log("Sadece ali gönderebilir")
-    next()
-}
-
-//app.use(customMiddleware)
-
-app.get('/',(req,res) => {
-    console.log("Home Page")
-    res.send("Home Page")
+mongoose.connect(MONGOURI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
 })
 
-app.get('/createPost',customMiddleware,(req,res) => {
-    res.send("send a post")
+mongoose.connection.on('connected',() => {
+    console.log("mongoose çalışıyor")
 })
+
+mongoose.connection.on('error',(err) => {
+    console.log("mongoose çalışmıyor",err)
+})
+
+// app.get('/',(req,res) => {
+//     Post.save(req.body)
+// })
+
+
 
 app.listen(PORT,() => {
     console.log("Server running on ", PORT)
---
+++ keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MONGOURI: "mongodb+srv://emre:XXkQFM7VjchLT14W@cluster0.vz2em.mongodb.net/Cluster0?retryWrites=true&w=majority"
+}
